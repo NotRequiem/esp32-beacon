ed9449e20af7f2644313b45e25bc4c7864053f2b
Requiem <114197630+NotRequiem@users.noreply.github.com>
2026-08-26 20:25:38 +0200

    feat: host store and utility firmware

---
+++ libraries/NimBLE-Arduino/nimble/nimble/host/store/config/src/ble_store_config_conf.c.d
@@ -0,0 +1,2 @@
+C:\Users\User\AppData\Local\arduino\sketches\FF609B428F878B6A31AA3E02D30DEBD1\libraries\NimBLE-Arduino\nimble\nimble\host\store\config\src\ble_store_config_conf.c.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\host\store\config\src\ble_store_config_conf.c
---
+++ libraries/NimBLE-Arduino/nimble/nimble/host/store/config/src/ble_store_config_conf.c.libsdetect.d
@@ -0,0 +1,2 @@
+ble_store_config_conf.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\host\store\config\src\ble_store_config_conf.c
