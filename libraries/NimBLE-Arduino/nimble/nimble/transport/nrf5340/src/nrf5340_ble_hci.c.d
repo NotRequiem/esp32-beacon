589d84a2fab879918dc65243e478528334d6791c
Requiem <114197630+NotRequiem@users.noreply.github.com>
2026-08-26 20:23:27 +0200

    feat: transport

---
+++ libraries/NimBLE-Arduino/nimble/nimble/transport/nrf5340/src/nrf5340_ble_hci.c.d
@@ -0,0 +1,2 @@
+C:\Users\User\AppData\Local\arduino\sketches\FF609B428F878B6A31AA3E02D30DEBD1\libraries\NimBLE-Arduino\nimble\nimble\transport\nrf5340\src\nrf5340_ble_hci.c.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\transport\nrf5340\src\nrf5340_ble_hci.c
---
+++ libraries/NimBLE-Arduino/nimble/nimble/transport/nrf5340/src/nrf5340_ble_hci.c.libsdetect.d
@@ -0,0 +1,2 @@
+nrf5340_ble_hci.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\transport\nrf5340\src\nrf5340_ble_hci.c
