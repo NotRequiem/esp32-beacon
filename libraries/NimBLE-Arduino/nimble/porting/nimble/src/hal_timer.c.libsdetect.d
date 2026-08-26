ecbd4d5004efd7a6200d3a51ea25823cffef3a29
Requiem <114197630+NotRequiem@users.noreply.github.com>
2026-08-26 20:20:43 +0200

    feat: hardware timers

---
+++ libraries/NimBLE-Arduino/nimble/porting/nimble/src/hal_timer.c.libsdetect.d
@@ -0,0 +1,2 @@
+hal_timer.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\porting\nimble\src\hal_timer.c
---
+++ libraries/NimBLE-Arduino/nimble/porting/nimble/src/os_cputime_pwr2.c.libsdetect.d
@@ -0,0 +1,10 @@
+os_cputime_pwr2.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\porting\nimble\src\os_cputime_pwr2.c \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/os/os_cputime.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/syscfg/syscfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/esp_port/port/include/esp_nimble_cfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig.h \
+ C:\Users\User\AppData\Local\Arduino15\packages\esp32\tools\esp32-libs\3.3.11/qio_qspi/include/sdkconfig.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig_rename.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/hal/hal_timer.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/os/queue.h
