ed93db41c2351e815af1d81c13e3175cf1996fbb
Requiem <114197630+NotRequiem@users.noreply.github.com>
2026-08-26 20:31:20 +0200

    feat BLE subnet source

---
+++ libraries/NimBLE-Arduino/nimble/nimble/host/mesh/src/subnet.c.d
@@ -0,0 +1,6 @@
+C:\Users\User\AppData\Local\arduino\sketches\FF609B428F878B6A31AA3E02D30DEBD1\libraries\NimBLE-Arduino\nimble\nimble\host\mesh\src\subnet.c.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\host\mesh\src\subnet.c \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/syscfg/syscfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/esp_port/port/include/esp_nimble_cfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig_rename.h
---
+++ libraries/NimBLE-Arduino/nimble/nimble/host/mesh/src/subnet.c.libsdetect.d
@@ -0,0 +1,6 @@
+subnet.o: \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src\nimble\nimble\host\mesh\src\subnet.c \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/porting/nimble/include/syscfg/syscfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimble/esp_port/port/include/esp_nimble_cfg.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig.h \
+ c:\Users\User\Documents\Arduino\libraries\NimBLE-Arduino\src/nimconfig_rename.h
